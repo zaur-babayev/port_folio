a3c6fb84e21d971d1d47c14a998b358a2a30dfbc
zaur-babayev <mail@zaurb.com>
2024-12-30 21:05:00 +0200

    Refresh bug

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'images.unsplash.com'],
   },
+  webpack: (config, { isServer }) => {
+    // Add custom webpack config here if needed
+    return config;
+  },
+  // Disable some features that might cause issues
+  swcMinify: false,
+  experimental: {
+    // Disable experimental features
+    esmExternals: false
+  }
 }
 
 module.exports = nextConfig
